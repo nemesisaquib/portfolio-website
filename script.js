f62c254cf90081b8ed1574cbb3866fcecf07a467
nemesis <nemesisgraphic19@gmail.com>
2021-09-29 18:32:42 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+let navBar = document.querySelector(".navbar");
+let body = document.querySelector("body");
+
+
+menuBtn.onclick = function () {
+  menuBtn.style.opacity = "0";
+  menuBtn.style.opinterEvents = "none";
+  navBar.classList.add("active");
+  body.style.overflow = "hidden"
+
+}
+
+cancelBtn.onclick = function () {
+  menuBtn.style.opacity = "1";
+  menuBtn.style.opinterEvents = "auto";
+  navBar.classList.remove("active");
+  body.style.overflow = "auto"
+}
+
+let nav = document.querySelector("nav");
+
+let val;
+
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
+
+let navLink = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLink.length; i++) {
+  navLink[i].addEventListener("click", () => {
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+    navBar.classList.remove("active");
+    body.style.overflow = "auto";
+  });
+}
